7dd4f6f9471388d7493de249c7bb5cc1f2c62260
Hazzatur <hazzatur@outlook.com>
2022-09-30 00:21:19 -0500

    Update EOS install files

--- user_commands.bash
+++ user_commands.bash
@@ -3,16 +3,17 @@
 username="$1"
 
 curl -o /home/$username/EOSNotes https://raw.githubusercontent.com/Hazzatur/Notes/main/EndeavourOSNotes
-chown -R $username:$username EOSNotes
+chown $username:$username /home/$username/EOSNotes
+
+mkdir -p /home/$username/{MEGA,Personal,Work}
+chown -R $username:$username /home/$username/{MEGA,Personal,Work}
 
 # [GRUB]
 sed -i "s/GRUB_TIMEOUT_STYLE=.*/GRUB_TIMEOUT_STYLE=hidden/g" /etc/default/grub
 
-# [Steam]
+# [Wine dependency hell]
 pacman -S --noconfirm --needed nvidia-dkms nvidia-utils lib32-nvidia-utils nvidia-settings vulkan-icd-loader lib32-vulkan-icd-loader
-pacman -S --noconfirm wine-staging winetricks
 pacman -S --noconfirm giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo libxcomposite lib32-libxcomposite libxinerama lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader cups samba dosbox
-pacman -S --noconfirm steam
 
 # [LightDM]
 curl -o /home/$username/Pictures/Wallpapers/custom.png --create-dirs https://cdna.artstation.com/p/assets/images/images/035/982/154/large/james-arkwright-jamesarkwright-disperse-02-jpg.jpg?1616715433
@@ -30,6 +31,9 @@ systemctl enable containerd.service
 # [OBS]
 usermod -aG video $username
 
+# [Bluetooth]
+systemctl enable bluetooth
+
 # [QEMU]
 yes | pacman -S qemu-full virt-manager virt-viewer vde2 bridge-utils openbsd-netcat libguestfs iptables-nft dnsmasq nftables
 
@@ -37,12 +41,7 @@ sed -i "s/#unix_sock_group*/unix_sock_group/g" /etc/libvirt/libvirtd.conf
 sed -i "s/#unix_sock_rw_perms*/unix_sock_rw_perms/g" /etc/libvirt/libvirtd.conf
 usermod -aG libvirt $username
 systemctl enable libvirtd.service
+newgrp libvirt
 virsh net-start default
 virsh net-autostart default
-
-# [Games]
-pacman -S --noconfirm retroarch ppsspp pcsx2 lutris
-
-# [Bluetooth]
-systemctl enable bluetooth
 
