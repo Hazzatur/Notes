60344b02ceb95a852d34075123a57d7cc8ba9666
Hazzatur <hazzatur@hazzatur.com>
2022-11-06 18:34:32 -0600

    EOS install script: change wallpaper curl

--- user_commands.bash
+++ user_commands.bash
@@ -72,9 +72,7 @@ pacman -S --noconfirm --noprogressbar --needed --disable-download-timeout nvidia
 pacman -S --noconfirm --noprogressbar --needed --disable-download-timeout giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo libxcomposite lib32-libxcomposite libxinerama lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader cups samba dosbox
 
 # [LightDM]
-curl -o /home/$username/Pictures/Wallpapers/custom.png --create-dirs $wallpaper
-chown -R $username:$username /home/$username/Pictures/
-cp /home/$username/Pictures/Wallpapers/custom.png /usr/share/backgrounds/custom.png
+curl -o /usr/share/backgrounds/custom.png --create-dirs $wallpaper
 sed -i "s,background=.*,background=/usr/share/backgrounds/custom.png,g" /etc/lightdm/slick-greeter.conf
 sed -i "s,show-power=.*,show-power=true,g" /etc/lightdm/slick-greeter.conf
 sed -i "s,background-color=.*,background-color=#000000,g" /etc/lightdm/slick-greeter.conf
