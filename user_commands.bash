d34f65f322559e81d41d48557fc75883f5e06721
Hazzatur <hazzatur@hazzatur.com>
2022-11-06 21:19:51 -0600

    EOS install script: minor fix

--- user_commands.bash
+++ user_commands.bash
@@ -38,18 +38,20 @@ EOT
   # [Monitor config]
   curl -o /etc/X11/edid.bin --create-dirs https://raw.githubusercontent.com/Hazzatur/Notes/main/edid.bin
   mkdir -p /home/$username/.screenlayout
-  chown -R $username:$username /home/$username/.screenlayout
-  sed -i "s,#display-setup-script=.*,display-setup-script=/home/$username/.screenlayout/monitor.sh,g" /etc/lightdm/lightdm.conf
   tee -a /home/$username/.screenlayout/monitor.sh > /dev/null <<EOT
 #!/bin/sh
 xrandr --output DP-0 --off --output DP-1 --off --output HDMI-0 --mode 1920x1080 --pos 0x310 --rotate normal --output DP-2 --mode 1920x1080 --pos 3840x0 --rotate left --output DP-3 --off --output HDMI-1 --off --output DP-4 --off --output DP-5 --primary --mode 1920x1080 --pos 1920x290 --rotate normal
 EOT
+  chmod +x /home/$username/.screenlayout/monitor.sh
+  chown -R $username:$username /home/$username/.screenlayout
+  sed -i "s,#display-setup-script=.*,display-setup-script=/home/$username/.screenlayout/monitor.sh,g" /etc/lightdm/lightdm.conf
 
   # [TL-WN823N]
   echo -e "blacklist rtl8xxxu" | tee -a /etc/modprobe.d/blacklist.conf
 else
   # Folders
   declare -a folders=(
+    "Calibre Library"
     "MEGA"
     "Personal"
     "Work"
@@ -79,14 +81,14 @@ sed -i "s,background-color=.*,background-color=#000000,g" /etc/lightdm/slick-gre
 
 # [Docker]
 usermod -aG docker $username
-systemctl enable docker
+systemctl enable docker.service
 systemctl enable containerd.service
 
 # [OBS]
 usermod -aG video $username
 
 # [Bluetooth]
-systemctl enable bluetooth
+systemctl enable bluetooth.service
 
 # [QEMU]
 yes | pacman -S --noprogressbar --needed --disable-download-timeout qemu-full virt-manager virt-viewer vde2 bridge-utils openbsd-netcat libguestfs iptables-nft dnsmasq nftables
